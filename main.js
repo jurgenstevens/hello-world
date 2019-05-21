f7de9b09ae03c5bfd60e8237eb51df0babf3e726
jurgenstevens <48702365+jurgenstevens@users.noreply.github.com>
2019-05-21 04:08:50 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 console.log("Up and running!");
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
 var cards = ["queen", "queen", "king", "king"];
-
 var cardsInPlay = [];
-var cardOne = ["queen"];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
 
-var cardTwo = ["queen"];
-cardsInPlay.push(cardTwo);
-console.log("User flipped queen");
-
-if(cardsInPlay.length === 2){ 
-	console.log("you have selected two cards"); 
-	if (cardsInPlay[0] === cardsInPlay[1]) 
-		alert("You found a match!"); 
-	else 
-		alert("Sorry, try again"); 
+var checkForMatch = function() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log('You found a match!');
+    } else {
+        console.log('Sorry, try again.');
+    }   
 }
+var flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[0]);
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+}
+
+flipCard(0);
+flipCard(1);
